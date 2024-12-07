618fd1831d89d0aac6b1689b65b831a5e05ae627
YYJ <1281074136@qq.com>
2024-12-07 19:04:38 +0800

    1

--- pdf2zh/high_level.py
+++ pdf2zh/high_level.py
@@ -1,6 +1,6 @@
 """Functions that can be used for the most common use-cases for pdf2zh.six"""
 
-from typing import BinaryIO
+from typing import BinaryIO, List, Tuple
 import numpy as np
 import tqdm
 from pymupdf import Document
@@ -11,6 +11,38 @@ from pdfminer.pdfparser import PDFParser
 from pdf2zh.converter import TranslateConverter
 from pdf2zh.pdfinterp import PDFPageInterpreterEx
 from pymupdf import Font
+
+
+def _process_layout_box(d, h: int, w: int) -> Tuple[int, int, int, int]:
+    """处理布局框的坐标"""
+    x0, y0, x1, y1 = d.xyxy.squeeze()
+    return (
+        np.clip(int(x0 - 1), 0, w - 1),
+        np.clip(int(h - y1 - 1), 0, h - 1),
+        np.clip(int(x1 + 1), 0, w - 1),
+        np.clip(int(h - y0 + 1), 0, h - 1),
+    )
+
+
+def _process_page_layout(page_layout, pix_height: int, pix_width: int) -> np.ndarray:
+    """处理页面布局，返回布局矩阵"""
+    box = np.ones((pix_height, pix_width))
+    h, w = box.shape
+    vcls = ["abandon", "figure", "table", "isolate_formula", "formula_caption"]
+    
+    # 处理非特殊区域
+    for i, d in enumerate(page_layout.boxes):
+        if not page_layout.names[int(d.cls)] in vcls:
+            x0, y0, x1, y1 = _process_layout_box(d, h, w)
+            box[y0:y1, x0:x1] = i + 2
+    
+    # 处理特殊区域
+    for d in page_layout.boxes:
+        if page_layout.names[int(d.cls)] in vcls:
+            x0, y0, x1, y1 = _process_layout_box(d, h, w)
+            box[y0:y1, x0:x1] = 0
+            
+    return box
 
 
 def extract_text_to_fp(
@@ -31,69 +63,48 @@ def extract_text_to_fp(
     noto: Font = None,
     callback: object = None,
     **kwarg,
-) -> None:
+) -> dict:
+    """Extract and process text from PDF file"""
+    layout = {}  
+    obj_patch = {}
     rsrcmgr = PDFResourceManager()
-    layout = {}
     device = TranslateConverter(
         rsrcmgr, vfont, vchar, thread, layout, lang_in, lang_out, service, resfont, noto
     )
-
     assert device is not None
-    obj_patch = {}
+    
     interpreter = PDFPageInterpreterEx(rsrcmgr, device, obj_patch)
-    if pages:
-        total_pages = len(pages)
-    else:
-        total_pages = page_count
-
+    total_pages = len(pages) if pages else page_count
+    
     parser = PDFParser(inf)
     doc = PDFDocument(parser, password=password)
-    with tqdm.tqdm(
-        enumerate(PDFPage.create_pages(doc)),
-        total=total_pages,
-    ) as progress:
-        for pageno, page in progress:
-            if pages and (pageno not in pages):
-                continue
-            if callback:
-                callback(progress)
-            page.pageno = pageno
-            pix = doc_en[page.pageno].get_pixmap()
-            image = np.fromstring(pix.samples, np.uint8).reshape(
-                pix.height, pix.width, 3
-            )[:, :, ::-1]
-            page_layout = model.predict(image, imgsz=int(pix.height / 32) * 32)[0]
-            # kdtree 是不可能 kdtree 的，不如直接渲染成图片，用空间换时间
-            box = np.ones((pix.height, pix.width))
-            h, w = box.shape
-            vcls = ["abandon", "figure", "table", "isolate_formula", "formula_caption"]
-            for i, d in enumerate(page_layout.boxes):
-                if not page_layout.names[int(d.cls)] in vcls:
-                    x0, y0, x1, y1 = d.xyxy.squeeze()
-                    x0, y0, x1, y1 = (
-                        np.clip(int(x0 - 1), 0, w - 1),
-                        np.clip(int(h - y1 - 1), 0, h - 1),
-                        np.clip(int(x1 + 1), 0, w - 1),
-                        np.clip(int(h - y0 + 1), 0, h - 1),
-                    )
-                    box[y0:y1, x0:x1] = i + 2
-            for i, d in enumerate(page_layout.boxes):
-                if page_layout.names[int(d.cls)] in vcls:
-                    x0, y0, x1, y1 = d.xyxy.squeeze()
-                    x0, y0, x1, y1 = (
-                        np.clip(int(x0 - 1), 0, w - 1),
-                        np.clip(int(h - y1 - 1), 0, h - 1),
-                        np.clip(int(x1 + 1), 0, w - 1),
-                        np.clip(int(h - y0 + 1), 0, h - 1),
-                    )
-                    box[y0:y1, x0:x1] = 0
-            layout[page.pageno] = box
-            # 新建一个 xref 存放新指令流
-            page.page_xref = doc_en.get_new_xref()  # hack 插入页面的新 xref
-            doc_en.update_object(page.page_xref, "<<>>")
-            doc_en.update_stream(page.page_xref, b"")
-            doc_en[page.pageno].set_contents(page.page_xref)
-            interpreter.process_page(page)
+    pages_iterator = enumerate(PDFPage.create_pages(doc))
+    
+    if not callback:
+        pages_iterator = tqdm.tqdm(pages_iterator, total=total_pages)
+
+    for pageno, page in pages_iterator:
+        if pages and pageno not in pages:
+            continue
+            
+        if callback:
+            callback({"total": total_pages, "current": pageno + 1})
+            
+        # 处理页面
+        page.pageno = pageno
+        pix = doc_en[pageno].get_pixmap()
+        image = np.frombuffer(pix.samples, np.uint8).reshape(pix.height, pix.width, 3)[:, :, ::-1]
+        page_layout = model.predict(image, imgsz=int(pix.height / 32) * 32)[0]
+        
+        # 处理布局
+        layout[pageno] = _process_page_layout(page_layout, pix.height, pix.width)
+        
+        # 更新页面内容
+        page.page_xref = doc_en.get_new_xref()
+        doc_en.update_object(page.page_xref, "<<>>")
+        doc_en.update_stream(page.page_xref, b"")
+        doc_en[pageno].set_contents(page.page_xref)
+        interpreter.process_page(page)
 
     device.close()
     return obj_patch
